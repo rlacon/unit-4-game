bc092736c2a7f587404233c2833f1c3472fe27c7
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-01 22:22:57 -0400

    Made crystals clickable

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var matchScore = 0;
+var wins = 10;
+var losses = 0;
+var totalScore = 0;
+
+
+//---------------ON PAGE LOAD---------------//
+
+function beginGame() {
+    console.log("Starting a new game!");
+
+}
+
